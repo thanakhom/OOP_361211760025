2a41d5e4b8e78526049acd98418ec58a0cf317af
khom <thanakom51@gmail.com>
2019-12-08 11:01:25 +0700

    Initial commit

---
+++ src/lab1/Hello.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Heloo World,I am Khom");
+
+
+
+
+
+
+
+
+
+
+
+    }//main
+
+}//class
