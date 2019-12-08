7021bb631b7f027203d0fb79a130d0c5c94137bf
khom <thanakom51@gmail.com>
2019-12-08 11:11:25 +0700

    Add message

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Heloo World,I am Khom");
+        System.out.println("MIT");
+        System.out.println("Faculty of Management Technology");
 
 
 
