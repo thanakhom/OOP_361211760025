3b650703c56e63cc323f13752a4a7949f9c138bf
khom <thanakom51@gmail.com>
2019-12-11 10:09:19 +0700

    Add message

---
+++ src/lab2/basicJava1.java
@@ -0,0 +1,46 @@
+package lab2;
+
+public class basicJava1 {
+    public static void main(String[] args) {
+
+        //variables
+        //integer
+        int x;
+        x=10;
+        System.out.println(x);
+        int y= -100;
+        System.out.println(y);
+        System.out.println(x+y);
+
+        //floating
+        float f=10.00f;
+        double d=100.12345;
+        System.out.println(f);
+        System.out.println(d);
+        double n= f*d;
+        System.out.println(n);
+
+        //charetor
+        char c='h';
+        System.out.println(c);
+        System.out.println(c+"Hello");
+
+        //String
+        String s="Thanakhom  Thongmee";
+        System.out.println(s);
+        String msg="@Saiyai";
+        String t= s+msg;
+        System.out.println(t);
+        System.out.println(t.length());
+
+        System.out.println(s.concat(msg));
+
+        System.out.println(t.toUpperCase());
+
+
+
+
+    }
+
+
+}//class
