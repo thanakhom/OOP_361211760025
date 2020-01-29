bf38283106f3658ac5d4f8fe427146229c7233a9
khom <thanakom51@gmail.com>
2020-01-29 09:02:14 +0700

    lab6

---
+++ src/lab6/exString.java
@@ -0,0 +1,43 @@
+package lab6;
+
+public class exString {
+    //Sring  เป็นข้อมูลชนิกข้อความ แต่ในภาภษาจาวา Sring เป็นคลาส
+    //ดังนั้นการประกาศตัวแปรชนิด Sring (เป็นการประกาศ opject ของคลาส)
+    //จึงสามารถเรียกใช้เมธตอดต่างๆ ที่อยู่ในคลาส Sring ได้
+
+    public static void main(String[] args) {
+
+        String msg = "Hello,Saiyai !!!"
+
+        // length ()  ใช้ในการนับจำนวนตัวอักษรในข้อความ ส่งค่ากลับเป็นจำนวนเต็ม (int)
+                int len = msg.length();
+                System.out.println(len);
+
+                System.out.println(msg.to);
+
+
+
+
+
+
+
+
+
+
+
+    }//main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//class
